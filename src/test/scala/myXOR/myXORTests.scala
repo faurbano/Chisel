7b10fd8ac64b3d73232ca18283d1663e5e771f85
Fernando Urbano <faurbano@gmail.com>
2025-03-14 14:29:53 -0500

    fixed myXORTest file

--- src/test/scala/myXOR/myXORTests.scala
+++ src/test/scala/myXOR/myXORTests.scala
@@ -6,17 +6,6 @@ import chiseltest._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class Simple extends Module{
-    val io = IO(new Bundle {
-        val a = Input (UInt(2.W))
-        val b = Input (UInt(2.W))
-        val out = Output (UInt(2.W))
-        val equ = Output (Bool())
-    })
-
-    io.out := io.a & io.b
-    io.equ := io.a === io.b
-}
 
 class myXORTest extends AnyFlatSpec with ChiselScalatestTester {
   "myXOR" should "pass" in {
